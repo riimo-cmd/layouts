34551cc439e87dfed5cd7519b9c332add51564f3
riimo-cmd <76709902+riimo-cmd@users.noreply.github.com>
2024-03-05 09:31:34 -0600

    pain and suffering (added links to ads)

--- 96windows/scripts.js
+++ 96windows/scripts.js
@@ -6,9 +6,14 @@ let neo2 = "./images/neocities.png";
 let neo3 = "./images/look-at-my-cat.png";
 
 let small1 = "./images/mdn.png";
+let link1 = "https://developer.mozilla.org/en-US/docs/Web"
 let small2 = "./images/w3schools.png";
+let link2 = "https://www.w3schools.com"
 let small3 = "./images/ia.png";
+let link3 = "https://archive.org"
 let small4 = "./images/gifcities.png";
+let link4 = "https://gifcities.org"
+let neocities = "https://neocities.org";
 
 
 
@@ -17,16 +22,26 @@ let img1 = document.createElement("img");
 if(largeBanner === 0){
     img1.src = neo1;
     let src = document.getElementById("neocities");
+    img1.onclick = function() {
+        window.location.href = neocities;
+    };
     src.appendChild(img1);
 }
 else if(largeBanner === 1){
     img1.src = neo2;
     let src = document.getElementById("neocities");
+    img1.onclick = function() {
+        window.location.href = neocities;
+    };
     src.appendChild(img1);
+    
 }
 else {
     img1.src = neo3;
     let src = document.getElementById("neocities");
+    img1.onclick = function() {
+        window.location.href = neocities;
+    };
     src.appendChild(img1);
 }
 
@@ -37,21 +52,33 @@ let img2 = document.createElement("img");
 if(smallBanner === 0){
     img2.src = small1;
     let src = document.getElementById("small1");
+    img2.onclick = function() {
+        window.location.href = link1;
+    };
     src.appendChild(img2);
 }
 else if(smallBanner === 1){
     img2.src = small2;
     let src = document.getElementById("small1");
+    img2.onclick = function() {
+        window.location.href = link2;
+    };
     src.appendChild(img2);
 }
 else if(smallBanner === 2){
     img2.src = small3;
     let src = document.getElementById("small1");
+    img2.onclick = function() {
+        window.location.href = link3;
+    };
     src.appendChild(img2);
 }
 else {
     img2.src = small4;
     let src = document.getElementById("small1");
+    img2.onclick = function() {
+        window.location.href = link4;
+    };
     src.appendChild(img2);
 }
 
@@ -62,21 +89,33 @@ let img3 = document.createElement("img");
 if(smallBanner === 0){
     img3.src = small1;
     let src = document.getElementById("small2");
+    img3.onclick = function() {
+        window.location.href = link1;
+    };
     src.appendChild(img3);
 }
 else if(smallBanner === 1){
     img3.src = small2;
     let src = document.getElementById("small2");
+    img3.onclick = function() {
+        window.location.href = link2;
+    };
     src.appendChild(img3);
 }
 else if(smallBanner === 2){
     img3.src = small3;
     let src = document.getElementById("small2");
+    img3.onclick = function() {
+        window.location.href = link3;
+    };
     src.appendChild(img3);
 }
 else {
     img3.src = small4;
     let src = document.getElementById("small2");
+    img3.onclick = function() {
+        window.location.href = link4;
+    };
     src.appendChild(img3);
 }
 
@@ -87,20 +126,32 @@ let img4 = document.createElement("img");
 if(smallBanner === 0){
     img4.src = small1;
     let src = document.getElementById("small3");
+    img4.onclick = function() {
+        window.location.href = link1;
+    };
     src.appendChild(img4);
 }
 else if(smallBanner === 1){
     img4.src = small2;
     let src = document.getElementById("small3");
+    img4.onclick = function() {
+        window.location.href = link2;
+    };
     src.appendChild(img4);
 }
 else if(smallBanner === 2){
     img4.src = small3;
     let src = document.getElementById("small3");
+    img4.onclick = function() {
+        window.location.href = link3;
+    };
     src.appendChild(img4);
 }
 else {
     img4.src = small4;
     let src = document.getElementById("small3");
+    img4.onclick = function() {
+        window.location.href = link4;
+    };
     src.appendChild(img4);
 }
